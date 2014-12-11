442acbd2bb99c54c4ac6b565a4ec67bc221489ee
jonaslading <j_lading@hotmail.com>
2014-12-11 11:39:29 +0100

    clear all now works

--- app/scripts/controllers/ToolbarCtrl.js
+++ app/scripts/controllers/ToolbarCtrl.js
@@ -5,6 +5,24 @@ app.controller('ToolbarCtrl', ['$scope', function ($scope) {
   var Toolbar = this;
 
 
+  Toolbar.clearAll = function(){
+
+    angular.forEach($scope.$parent.Plan.plandata.planlaegger.kompetenceomraader.kompetenceomraade, function(omraade){
+      angular.forEach(omraade.faerdighedsOgVidensmaalPLURALIS.faerdighedsOgvidensmaalSINGULARIS, function(vidensmaal){
+        vidensmaal.Selected = false;
+        if(vidensmaal.faser.fase.length) {
+          angular.forEach(vidensmaal.faser.fase, function (fase) {
+            fase.Selected = false;
+          });
+        }
+      });
+    });
+    angular.forEach($scope.$parent.Plan.courses, function(course){
+      course.highlight = false;
+    });
+
+  };
+
 
   //Test variable. If you see it when the app runs you are good to go
   Toolbar.testVar = 'This is data from the menubar!';
